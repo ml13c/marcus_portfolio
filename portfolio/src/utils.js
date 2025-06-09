dc9841e3454ecb062f6e59c916dc539010e8add9
ml13c <marcuslara240@gmail.com>
2025-06-09 10:56:55 -0500

    fixed image icon issue not loading

--- portfolio/src/utils.js
+++ portfolio/src/utils.js
@@ -1,3 +1,21 @@
+import collinCollegeImg from './assets/history/collin-college.jpg';
+import asmeImg from './assets/history/asme.png';
+const imageMap = {
+  // history images
+  'history/collin-college.jpg': collinCollegeImg,
+  'history/asme.png': asmeImg,
+  // 'history/other.jpg': otherImg,
+
+  // skills images
+ // 'skills/skill1.jpg': skill1Img,
+  //'skills/skill2.jpg': skill2Img
+  // add more as needed
+};
 export const getImageUrl = (path) => {
-  return new URL(`/assets/${path}`, import.meta.url).href;
+  const image = imageMap[path];
+  if (!image) {
+    console.warn(`Image not found for path: ${path}`);
+    return ''; // or a fallback image if you want
+  }
+  return image;
 };
