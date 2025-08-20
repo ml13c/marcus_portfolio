7791a59bb38b435ef33b7512f42071da4cb37b43
ml13c <marcuslara240@gmail.com>
2025-08-20 14:45:16 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/website-portfolio/", // note the trailing slash is important
+  base: "/marcus_portfolio/", // note the trailing slash is important
   css: {
     modules: {
       localsConvention: "camelCase" // lowercase "l"
